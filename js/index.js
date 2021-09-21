24d8038406c56e73cc02a84b0b705bd63160554f
Anton-Sakharov-hub <saharovanton357@gmail.com>
2021-09-21 16:58:06 +0300

    рефакторит код

--- js/index.js
+++ js/index.js
@@ -18,7 +18,6 @@ const colors = [
   '#795548',
 ];
 
-// let indexOfColor = 0;
 let timerId = null;
 
 const refs = {
@@ -27,61 +26,36 @@ const refs = {
     body: document.querySelector('body'),
 };
 
-// function changeIndex(index) {
-//     indexOfColor += 1;
-//     console.log(index);
-// };
-
-// changeIndex(indexOfColor);
-// changeIndex(indexOfColor);
-// changeIndex(indexOfColor);
-// changeIndex(indexOfColor);
-// changeIndex(indexOfColor);
-// console.log(indexOfColor);
-
-const randomIntegerFromInterval = (min, max) => {
+const randomNumberFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-function setBckgrncolorToBody(colors, randomIntegerFromInterval) {
-        // console.log(colors);
+function setBckgrdcolorToBody(colors, randomNumberFromInterval) {
     
-    console.log(refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, 5)]);
-    // console.log(indexOfColor);
-    // indexOfColor += 1;
-    // if (indexOfColor === 6) {
-    //     indexOfColor = 0;
-    // };
+    console.log(refs.body.style.backgroundColor = colors[randomNumberFromInterval(0, 5)]);
 };
-
-
-refs.buttonStart.addEventListener('click', () => {
-   
-    timerId = setInterval(setBckgrncolorToBody, 1000, colors, randomIntegerFromInterval);
-
-    if (timerId) {
-        refs.buttonStart.setAttribute('disabled', '');
-    };
-});
 
 function stopChangeColors() {
     clearTimeout(timerId);
     refs.buttonStart.removeAttribute('disabled');
 };
 
+function changeColorAtInterval() {
+    timerId = setInterval(setBckgrdcolorToBody, 1000, colors, randomNumberFromInterval);
+
+    if (timerId) {
+        refs.buttonStart.setAttribute('disabled', '');
+    };
+};
+
+
+refs.buttonStart.addEventListener('click', changeColorAtInterval);
+
 refs.buttonStop.addEventListener('click', stopChangeColors);
 
 
 
-// setInterval(setBckgrncolorToBody, 1000, colors, indexOfColor)
 
-// setBckgrncolorToBody(colors, index)
-
-// function setTimer(colors) {
-//     const timer = setInterval(setBckgrncolorToBody, 1000, colors);
-// };
-
-// setTimer();
 
 
 
